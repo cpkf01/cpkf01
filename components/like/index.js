b5fef0f9c0301e4f1ad80d81800bfd52cd5b5e89
XaoCo <17600201849@163.com>
2019-01-18 15:52:57 +0800

    like组件实现点击切换图片和右上角数字的动态变化

--- components/like/index.js
+++ components/like/index.js
@@ -25,7 +25,14 @@ Component({
    */
   methods: {
     onLike: function (args) {
-      console.log(args);
+      let like = this.properties.like;
+      let count = this.properties.count;
+      count = like?count-1:count+1;
+      this.setData({
+        count:count,
+        like:!like
+
+      })
     }
   }
 })
