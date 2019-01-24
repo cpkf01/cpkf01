6bdd984c6f284276e53210adfe8df00edb0497cb
XaoCo <17600201849@163.com>
2019-01-24 11:24:02 +0800

    错误码方法以及使用

--- util/http.js
+++ util/http.js
@@ -1,8 +1,15 @@
 // 导入config.js 里的东西
 import {config} from '../config.js'
+
+const tips = {
+    1:'抱歉,服务器离家出走了',
+    1005:'appkey无效，请前往www.7yue.pro申请',
+    3000:'期刊不存在'
+}
+
 // 用es6语法,创建一个类
 class HTTP {
-// HTTP 是类名
+// request方法
     request(params){
 // request 是方法名
         wx.request({
@@ -21,9 +28,11 @@ class HTTP {
                 let code = res.statusCode.toString();
 
                 if (code.startsWith('2')) {
-                    
+                    params.success(res.data)
                 } else {
-                    
+                    let error_code = res.data.error_code;
+                    console.log(error_code);
+                   this._show_error(error_code)
                 }
             },
             fail:(err)=>{
@@ -31,5 +40,19 @@ class HTTP {
             }
         })
     }
+// 错误码方法
+    _show_error(error_code){
+        // 如果错误码不存在时,默认错误码是1
+        if(!error_code){
+            error_code = 1
+        }
+        wx.showToast({
+            title:tips[error_code],
+            icon:'none',
+            duration:2000
+
+        })
+    }
+
 }
 export {HTTP}
